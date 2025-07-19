857ebc38365657af45da3cef265683f2c9713853
David Alexander Toapanta Toapanta <116773030+DavidAToapanta@users.noreply.github.com>
2025-07-19 16:51:07 -0500

    Creacion de componentes

--- hero-gym-web/src/app/pages/login/login.component.ts
+++ hero-gym-web/src/app/pages/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched()
+    }
+  }
 }
