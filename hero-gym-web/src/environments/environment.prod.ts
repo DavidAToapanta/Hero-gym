247d65a9d968cb9ab070eea4f153e01257ee362f
David Alexander Toapanta Toapanta <116773030+DavidAToapanta@users.noreply.github.com>
2026-04-06 00:17:14 -0500

    Api

--- hero-gym-web/src/environments/environment.prod.ts
+++ hero-gym-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backendhero-api.onrender.com'
+  apiUrl: 'https://backendhero-api-prod-328397609474.us-central1.run.app'
 };
--- hero-gym-web/src/environments/environment.ts
+++ hero-gym-web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backendhero-api.onrender.com'
+  apiUrl: 'https://backendhero-api-prod-328397609474.us-central1.run.app'
 };
